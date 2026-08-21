e093f7e6efd0e72b56ac3009064c3cf78e272b22
parthimp952 <parthimp952@gmail.com>
2026-08-21 17:40:35 +0530

    done

--- FrontEnd/src/services/api.ts
+++ FrontEnd/src/services/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://employee-1-665d.onrender.com/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL,  
   headers: {
     'Content-Type': 'application/json',
   },
